4d3ec7c4cec0f6a3b66b0ccdec850b0b769be973
Burkan Akıncı <burkanakinci@gmail.com>
2023-02-22 23:54:34 +0300

    Added Opponent Run State

--- Assets/Scripts/Objects/Miner/OpponentMiner.cs
+++ Assets/Scripts/Objects/Miner/OpponentMiner.cs
@@ -11,6 +11,7 @@ public class OpponentMiner : BaseMiner
 
         List<IMinerState> m_OpponentMinerStates = new List<IMinerState>();
         m_OpponentMinerStates.Add(new IdleOpponentMinerState(this));
+        m_OpponentMinerStates.Add(new RunOpponentMinerState(this));
 
         m_OpponentStateMachine = new OpponentMinerStateMachine(m_OpponentMinerStates);
     }
